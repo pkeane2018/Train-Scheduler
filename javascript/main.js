ae0d1461dc72b802b482c9181728d1a0ae24cf92
Patrick Keane <pkeane92@yahoo.com>
2018-11-01 18:45:36 -0500

    done with most of it

--- javascript/main.js
+++ javascript/main.js
@@ -55,15 +55,54 @@ console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
         console.log(destination);
         console.log(frequency);
     
-        database.ref().set({
+        database.ref().push({
             name : name,
             destination: destination,
             frequency: frequency,
-            dateAdded: firebase.database.ServerValue.TIMESTAMP
+            firstTime : firstTime,  
         });
 
+
     })    
-    
+
+    database.ref().on("child_added", function(snapshot) {
+        
+        var snap = snapshot.val();
+
+        console.log(snap.name);
+        console.log(snap.destination);
+        console.log(snap.frequency);
+        console.log(snap.firstTime);
+
+        var name = snap.name;
+        var destination = snap.destination;
+        var frequency = snap.frequency;
+        var firstTime = snap.firstTime;
+
+        var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+        console.log(firstTimeConverted);
+
+        var currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+
+        var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME: " + diffTime);
+
+        var tRemainder = diffTime % frequency;
+        console.log(tRemainder);
+
+        var tMinutesTillTrain = frequency - tRemainder;
+        console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+        var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
+        $("#currenttrains").append("<tr><td>" + name + "</td><td>" + destination + "</td><td>" + frequency + "</td><td>" +  moment(nextTrain).format("hh:mm") + "</td><td>" + tMinutesTillTrain + "</td></tr>");      
+
+    },function(errorObject) {
+        console.log("Errors handled: " + errorObject.code);
+
+    });
 }) 
 
 
